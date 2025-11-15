e049dce367b5fde3d24b69c188a49c7069e28e62
Tad Hunt <tadhunt@gmail.com>
2025-11-14 18:06:47 -0700

    disable hitting when players are inside the hide and seek arena but games aren't running

--- src/main/java/com/mcplaydates/hideAndSeek/util/Listeners/EntityHitListener.java
+++ src/main/java/com/mcplaydates/hideAndSeek/util/Listeners/EntityHitListener.java
@@ -1,6 +1,7 @@
 package com.mcplaydates.hideAndSeek.util.Listeners;
 
 import com.mcplaydates.hideAndSeek.HideAndSeek;
+import com.mcplaydates.hideAndSeek.util.Border;
 import com.mcplaydates.hideAndSeek.util.CoreGame.End;
 import com.mcplaydates.hideAndSeek.util.CoreGame.Game;
 import com.mcplaydates.hideAndSeek.util.CoreGame.Start;
@@ -17,12 +18,16 @@ public class EntityHitListener implements Listener {
     Start start;
     Game game;
     End end;
-    public EntityHitListener(HideAndSeek hs, Start start, Game game, End end){
+    Border border;
+
+    public EntityHitListener(HideAndSeek hs, Start start, Game game, End end, Border border) {
         this.hs = hs;
         this.start = start;
         this.game = game;
         this.end = end;
+        this.border = border;
     }
+
     @EventHandler
     public void onHit(EntityDamageByEntityEvent event){
         if(start.getIsHidingPhase()){
@@ -52,6 +57,30 @@ public class EntityHitListener implements Listener {
             end.checkGameOver();
 
             event.setCancelled(true);
+            return;
+        }
+
+        if (border.hasBorder()) {
+            // game isn't in progress, we disable hits inside the arena, but not elsewhere in the world
+            Player defender = null;
+            Player attacker = null;
+            if(event.getEntity() instanceof Player){
+                defender = (Player) event.getEntity();
+            }
+            if(event.getDamager() instanceof Player){
+                attacker = (Player) event.getDamager();
+            } else if(event.getDamager() instanceof Arrow && (((Arrow) event.getDamager()).getShooter() instanceof Player)){
+                attacker = (Player) (((Arrow) event.getDamager()).getShooter());
+            }
+
+            if (defender != null && border.isInBorder(defender.getLocation())) {
+                event.setCancelled(true);
+                return;
+            }
+            if (attacker != null && border.isInBorder(attacker.getLocation())) {
+                event.setCancelled(true);
+                return;
+            }
         }
     }
 }
